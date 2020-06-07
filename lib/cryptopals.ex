006c4177af3a632b94b7c4af950335751d67dd8d
Adam Sebzda <asebzda@gmail.com>
2020-06-07 16:59:49 -0400

    Challenge 2

--- lib/cryptopals.ex
+++ lib/cryptopals.ex
@@ -5,17 +5,34 @@ defmodule Cryptopals do
 
   @doc """
   Converts a hex string to base64
+  Assumes input is lowercase
 
   ## Examples
 
-      iex> Cryptopals.hex_to_base64("49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d")
-      "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t"
+      iex> Cryptopals.hex_to_base64("a8b419")
+      "qLQZ"
 
   """
   def hex_to_base64(input) when is_binary(input) do
     input
     |> Base.decode16!(case: :lower)
     |> Base.encode64()
+  end
+
+  @doc """
+  Performs an XOR on two hex strings of equal size
+  Assumes input is lowercase
+
+  ## Examples
+
+      iex> Cryptopals.fixed_xor("1c0111001f01010006", "1a024b53535009181c")
+      "6035a534c5108181a"
+
+  """
+  def fixed_xor(first, second) when is_binary(first) and is_binary(second) do
+    Bitwise.bxor(String.to_integer(first, 16), String.to_integer(second, 16))
+    |> Integer.to_string(16)
+    |> String.downcase
   end
 
 end
--- test/cryptopals_test.exs
+++ test/cryptopals_test.exs
@@ -2,7 +2,11 @@ defmodule CryptopalsTest do
   use ExUnit.Case
   doctest Cryptopals
 
-#  test "greets the world" do
-#    assert Cryptopals.hello() == :world
-#  end
+  test "Converting hex to base64" do
+    assert Cryptopals.hex_to_base64("49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d") == "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t"
+  end
+
+  test "Fixed XOR on hex" do
+    assert Cryptopals.fixed_xor("1c0111001f010100061a024b53535009181c", "686974207468652062756c6c277320657965") == "746865206b696420646f6e277420706c6179"
+  end
 end
