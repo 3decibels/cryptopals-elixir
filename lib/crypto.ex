aed28f3c7f547865586c294c500e694809328ac4
Adam Sebzda <asebzda@gmail.com>
2020-06-25 10:28:01 -0400

    AES CBC

--- lib/crypto.ex
+++ lib/crypto.ex
@@ -26,4 +26,32 @@ defmodule Cryptopals.Crypto do
 
   def pad_pkcs7(data, blocksize) when is_binary(data) and is_integer(blocksize), do: data
 
+
+  @doc """
+  Encrypts or decrypts data using AES in CBC mode.
+
+  Takes in data, the encryption key, the IV and an atom specifying if the function is encrypting or decrypting (:encrypt or :decrypt)
+  """
+  def aes_cbc(data, key, iv, direction) when is_binary(data) and is_binary(key) and is_binary(iv) and is_atom(direction) and byte_size(key) == byte_size(iv) do
+    encrypt =
+      cond do
+        direction == :encrypt -> true
+        direction == :decrypt -> false
+        true -> raise ArgumentError, message: "Must pass either :encrypt or :decrypt as the direction"
+      end
+    aes_cbc(data, key, iv, encrypt, <<>>)
+  end
+
+
+  defp aes_cbc(data, key, previous, encrypt, acc) when byte_size(data) >= byte_size(key) do
+    blocksize = byte_size(key)
+    <<block::bytes-size(blocksize), tail::binary>> = data
+    block = :crypto.exor(block, previous)
+    acc <> :crypto.crypto_one_time(:aes_128_ecb, key, block, encrypt)
+    aes_cbc(tail, key, block, encrypt, acc)
+  end
+
+
+  defp aes_cbc(_data, _key, _previous, _encrypt, acc), do: acc
+
 end
